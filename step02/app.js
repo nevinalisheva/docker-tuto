db656ffc6380f92a9c1a4c80deb1479c9adffe38
Leonard Flachs <leonard.flachs@gmail.com>
2022-01-06 11:50:19 +0100

    ✨ Add files

--- step02/app.js
+++ step02/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 
 const PORT = 3000;
+
+app.get("/", (req, res) => res.json({ ok: true }));
+
 app.listen(PORT, () => {
   console.log(`app is running on ${PORT}`);
 });
